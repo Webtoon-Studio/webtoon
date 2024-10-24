4e3b2afac2040b38e11b714277d929fd2ec3b2e9
Rolo <roloedits@gmail.com>
2024-10-23 19:28:56 -0700

    chore: clean up residual leftover commented code

--- src/platform/webtoons/webtoon/dashboard/comments.rs
+++
@@ -1,9 +0,0 @@
-// pub struct EpisodeInfo {
-//     pub result: Result,
-// }
-//
-// pub struct Result {
-//     age_grade_notice: Option<String>,
-//     unsuitable_for_children: Option<String>,
-//     title_unsuitable_for_children: bool,
-// }
