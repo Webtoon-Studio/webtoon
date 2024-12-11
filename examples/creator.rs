6b99e6b2852f48e1ef918577e15d2ae95a3feb34
Rolo <roloedits@gmail.com>
2024-12-11 03:13:23 -0800

    fix(creator): handle edgecase for old accounts

--- examples/creator.rs
+++ examples/creator.rs
@@ -1,17 +1,17 @@
 use anyhow::bail;
 use webtoon::platform::webtoons::{Client, Language};
 
-#[tokio::main(flavor = "current_thread")]
+#[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let client = Client::new();
 
-    let Some(creator) = client.creator("w7m5o", Language::En).await? else {
+    let Some(creator) = client.creator("JennyToons", Language::En).await? else {
         bail!("no creator exists with given id");
     };
 
-    println!("{}", creator.username());
-    println!("{:?}", creator.followers().await?);
-    println!("{:#?}", creator.webtoons().await?);
+    println!("username: {}", creator.username());
+    println!("followers: {:?}", creator.followers().await?);
+    println!("webtoons: {:#?}", creator.webtoons().await?);
 
     Ok(())
 }
