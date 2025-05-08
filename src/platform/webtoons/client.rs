18a63fa2d6d8fb14bbe0cdc8aa1bf707d9cb316b
rolo <roloedits@gmail.com>
2025-05-07 19:57:05 -0700

    refactor: switch `webtoons/client` to new `http` module

--- src/platform/webtoons/client.rs
+++ src/platform/webtoons/client.rs
@@ -4,6 +4,8 @@ pub(super) mod likes;
 pub(super) mod posts;
 pub mod search;
 
+use crate::stdx::http::IRetry;
+
 use super::{
     Language, Type, Webtoon,
     canvas::{self, Sort},
@@ -22,11 +24,11 @@ use super::{
 use anyhow::{Context, anyhow};
 use parking_lot::RwLock;
 use posts::id::Id;
-use reqwest::{RequestBuilder, Response};
+use reqwest::Response;
 use search::Item;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
-use std::{collections::HashMap, env, ops::RangeBounds, str::FromStr, sync::Arc, time::Duration};
+use std::{collections::HashMap, env, ops::RangeBounds, str::FromStr, sync::Arc};
 
 static APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
 
@@ -1688,48 +1690,4 @@ struct NewLikesResponse {
 #[serde(rename_all = "camelCase")]
 struct NewLikesResult {
     count: u32,
-}
-
-struct Retry(RequestBuilder);
-
-impl Retry {
-    pub async fn send(self) -> Result<Response, ClientError> {
-        let mut count = 10;
-        let mut wait = fastrand::u64(1..=10);
-
-        loop {
-            let request = self.0.try_clone().ok_or(ClientError::Unexpected(anyhow!(
-                "failed to clone `RequestBuilder` in retry loop"
-            )))?;
-            let response = request.send().await;
-
-            match response {
-                Ok(response) if response.status() == 429 && count != 0 => {
-                    tokio::time::sleep(Duration::from_secs(wait)).await;
-                    count -= 1;
-                    wait += 3;
-                    wait += fastrand::u64(1..=5);
-                }
-                Err(_) if count != 0 => {
-                    tokio::time::sleep(Duration::from_secs(wait)).await;
-                    count -= 1;
-                    wait += 3;
-                    wait += fastrand::u64(1..=5);
-                }
-
-                Ok(response) => return Ok(response),
-                Err(err) => return Err(err.into()),
-            }
-        }
-    }
-}
-
-trait IRetry {
-    fn retry(self) -> Retry;
-}
-
-impl IRetry for RequestBuilder {
-    fn retry(self) -> Retry {
-        Retry(self)
-    }
 }
