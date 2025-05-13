b1e78daec8aadba2522451357245899177d7c3b4
rolo <roloedits@gmail.com>
2025-05-13 00:42:25 -0700

    todo: add module doc TODO

--- src/platform/naver/mod.rs
+++ src/platform/naver/mod.rs
@@ -1,5 +1,10 @@
 //! An abstraction for the `comic.naver.com` website.
 
+// TODO: Add module docs on some of domain knowledge of how Naver structures its site and how the library abstracts it where possible
+//
+// Naver uses a single set of ids across all three "domains" of featured, challenge, and best challenge. With this, only
+// the id is needed to find a webtoon.
+
 pub mod client;
 pub mod creator;
 pub mod errors;
