b833b9b635580e91cdb6f073cfe042a9d6ec9946
Rolo <roloedits@gmail.com>
2025-11-24 16:20:23 -0800

    feat: add check for the number of episodes per page on dashboard

--- src/platform/webtoons/dashboard/episodes.rs
+++ src/platform/webtoons/dashboard/episodes.rs
@@ -15,6 +15,8 @@ use crate::{
 use std::{collections::HashSet, str::FromStr, time::Duration};
 
 pub async fn scrape(webtoon: &Webtoon) -> Result<Vec<Episode>, EpisodeError> {
+    const MAX_EPISODES_PER_PAGE: u16 = 10;
+
     #[expect(
         clippy::mutable_key_type,
         reason = "`Episode` has interior mutability, but `Hash` is only on the `number`"
@@ -23,13 +25,19 @@ pub async fn scrape(webtoon: &Webtoon) -> Result<Vec<Episode>, EpisodeError> {
 
     let dashboard_episodes = webtoon.client.get_episodes_dashboard(webtoon, 1).await?;
 
+    invariant!(
+        dashboard_episodes.len() <= usize::from(MAX_EPISODES_PER_PAGE),
+        "`webtoons.com` episode dashboard was expected to have a max of 10 per page, but had: {}",
+        dashboard_episodes.len()
+    );
+
     let pages = match dashboard_episodes.as_slice() {
         // A brand new Webtoon or a Webtoon with all episodes deleted would be empty.
         [] => return Ok(Vec::new()),
         // This gets the highest numerical episode number and calculates what page it
         // would be in, given max episodes per page value. This gives us how many
         // pages we need to go through.
-        [first, ..] => first.metadata.number.in_bucket_of(10),
+        [first, ..] => first.metadata.number.in_bucket_of(MAX_EPISODES_PER_PAGE),
     };
 
     for episode in dashboard_episodes {
