0a9b0e991a759becd85a6a8e51c2661e83181970
Rolo <roloedits@gmail.com>
2025-12-01 15:37:19 -0800

    refactor: sort pub exports differently in webtoons/mod

--- src/platform/webtoons/mod.rs
+++ src/platform/webtoons/mod.rs
@@ -3,14 +3,14 @@
 mod dashboard;
 mod search;
 
+pub mod canvas;
+pub mod originals;
+
 pub mod client;
 pub mod creator;
 pub mod error;
 pub mod meta;
 pub mod webtoon;
-
-pub mod canvas;
-pub mod originals;
 
 pub use client::{Client, ClientBuilder};
 pub use creator::Creator;
