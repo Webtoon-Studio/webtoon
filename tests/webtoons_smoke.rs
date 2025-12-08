209cabc9055a072e130543c5479d239c54014b30
Rolo <RoloEdits@gmail.com>
2025-12-07 18:35:51 -0800

    test: add originals smoke test and tweak test params

--- tests/webtoons_smoke.rs
+++ tests/webtoons_smoke.rs
@@ -1,11 +1,13 @@
-use webtoon::platform::webtoons::{Client, Language, canvas::Sort, webtoon::post::Posts};
+use webtoon::platform::webtoons::{
+    Client, Language, canvas::Sort, error::CreatorError, webtoon::post::Posts,
+};
 
 #[tokio::test]
 #[ignore]
 async fn canvas() {
     let client = Client::new();
 
-    let page = fastrand::u16(1..=10000);
+    let page = fastrand::u16(1..=5000);
 
     let canvas = client
         .canvas(Language::En, page..=page, Sort::Popularity)
@@ -15,7 +17,7 @@ async fn canvas() {
     assert_eq!(20, canvas.len());
 
     for webtoon in canvas {
-        eprintln!("\nChecking webtoon {}", webtoon.id());
+        eprintln!("\nChecking `Canvas` Webtoon {}", webtoon.id());
 
         let _title = webtoon.title().await.unwrap();
 
@@ -23,7 +25,7 @@ async fn canvas() {
         match creators.as_slice() {
             [] => unreachable!("every webtoon must have a creator"),
             [_first, _second, _rest @ ..] => {
-                unreachable!("`canvas stories can only have one creator")
+                unreachable!("canvas stories can only have one creator: {creators:?}")
             }
             [creator] => {
                 let _has_patreon = creator.has_patreon().await.unwrap();
@@ -35,6 +37,7 @@ async fn canvas() {
         match genres.as_slice() {
             [] => unreachable!("every webtoons must have a genre"),
             [_genre] => {}
+            // Can only have two genres assigned
             [_first, _second, rest @ ..] => {
                 assert!(rest.is_empty());
             }
@@ -69,7 +72,91 @@ async fn canvas() {
 
         episode
             .posts_for_each(async |post| {
-                eprintln!("Checking post: {}", post.id());
+                let _replies = post.replies::<u32>().await.unwrap();
+                let _replies = post.replies::<Posts>().await.unwrap();
+            })
+            .await
+            .unwrap();
+    }
+}
+
+#[tokio::test]
+#[ignore]
+async fn originals() {
+    let client = Client::new();
+
+    let originals = client.originals(Language::En).await.unwrap();
+
+    assert!(!originals.is_empty());
+
+    for _ in 1..=10 {
+        let idx = fastrand::usize(0..originals.len());
+        let webtoon = &originals[idx];
+
+        eprintln!("\nChecking `Original` Webtoon {}", webtoon.id());
+
+        let _title = webtoon.title().await.unwrap();
+
+        let creators = webtoon.creators().await.unwrap();
+        match creators.as_slice() {
+            [] => unreachable!("every webtoon must have a creator"),
+            creators => {
+                for creator in creators {
+                    // TODO: Need to verify that this works for Korean Creators.
+                    let _has_patreon = match creator.has_patreon().await {
+                        Ok(has_patreon) => has_patreon,
+                        Err(CreatorError::PageDisabledByCreator) => None,
+                        Err(err) => panic!("{err}"),
+                    };
+
+                    // TODO: Need to verify that this works for Korean Creators.
+                    let _id = match creator.id().await {
+                        Ok(id) => id,
+                        Err(CreatorError::PageDisabledByCreator) => None,
+                        Err(err) => panic!("{err}"),
+                    };
+                }
+            }
+        }
+
+        let genres = webtoon.genres().await.unwrap();
+        match genres.as_slice() {
+            [] => unreachable!("every webtoons must have a genre"),
+            [_genre] => {}
+            [_first, rest @ ..] => {
+                assert!(rest.is_empty());
+            }
+        }
+
+        let thumbnail = webtoon.thumbnail().await.unwrap();
+        assert!(thumbnail.is_none());
+
+        let banner = webtoon.banner().await.unwrap();
+        assert!(banner.is_some());
+
+        let language = webtoon.language();
+        assert_eq!(Language::En, language);
+
+        let schedule = webtoon.schedule().await.unwrap();
+        assert!(schedule.is_some());
+
+        let _views = webtoon.views().await.unwrap();
+
+        let _subscribers = webtoon.subscribers().await.unwrap();
+
+        let _summary = webtoon.summary().await.unwrap();
+
+        let episode = webtoon.random_episode().await.unwrap();
+        eprintln!("episode {}", episode.number());
+
+        let _title = episode.title().await.unwrap();
+        let _season = episode.season().await.unwrap();
+        let _thumbnail = episode.thumbnail().await.unwrap();
+        let _length = episode.length().await.unwrap();
+        let _likes = episode.likes().await.unwrap();
+
+        episode
+            .posts_for_each(async |post| {
                 let _replies = post.replies::<u32>().await.unwrap();
                 let _replies = post.replies::<Posts>().await.unwrap();
             })
